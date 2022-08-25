a4340a2b8a1c0866d1343386d2818a658cffd7c7
Priyanka-Shahi <shahipriyan@gmail.com>
2022-08-25 19:20:06 +1000

    Done.

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,24 +1,32 @@
 import React, { useState } from "react";
 
-function Item({ name, category }) {
-  const [isInCart, setIsInCart] = useState(false);
-
-  function handleAddToCartClick() {
-    setIsInCart((isInCart) => !isInCart);
+class Item extends React.Component {
+  //const [isInCart, setIsInCart] = useState(false);
+  state = {
+    isInCart: false,
   }
 
+  handleAddToCartClick = () => {
+    this.setState((prevState) => ({
+      isInCart: !prevState.isInCart,
+    }))
+  }
+ 
+  render() {
+    const {name, category} = this.props;
+  
   return (
-    <li className={isInCart ? "in-cart" : ""}>
+    <li className={this.state.isInCart ? "in-cart" : ""}>
       <span>{name}</span>
       <span className="category">{category}</span>
       <button
-        className={isInCart ? "remove" : "add"}
-        onClick={handleAddToCartClick}
+        className={this.state.isInCart ? "remove" : "add"}
+        onClick={this.handleAddToCartClick}
       >
-        {isInCart ? "Remove From" : "Add to"} Cart
+        {this.isInCart ? "Remove From" : "Add to"} Cart
       </button>
     </li>
-  );
+  )
 }
-
+}
 export default Item;
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,25 +1,29 @@
-import React, { useState } from "react";
+import React from "react";
 import Item from "./Item";
 
-function ShoppingList({ items }) {
-  const [selectedCategory, setSelectedCategory] = useState("All");
+class ShoppingList extends React.Component{
+  //const [selectedCategory, setSelectedCategory] = useState("All");
+  state = {
+    selectedCategory: 'All',
+  }
 
-  function handleCategoryChange(event) {
+  handleCategoryChange = (e) => {
     // event.target.value will be the value selected by the user
-    setSelectedCategory(event.target.value);
+    this.setState({selectedCategory: e.target.value})
   }
 
   // we want to filter the items to only display the ones based on the selected category
-  const itemsToDisplay = items.filter((item) => {
-    if (selectedCategory === "All") return true;
+  render() {
+  const itemsToDisplay = this.props.items.filter((item) => {
+    if (this.state.selectedCategory === "All") return true;
 
-    return item.category === selectedCategory;
-  });
+    return item.category === this.state.selectedCategory;
+  })
 
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter" onChange={handleCategoryChange}>
+        <select name="filter" onChange={this.handleCategoryChange}>
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -32,7 +36,7 @@ function ShoppingList({ items }) {
         ))}
       </ul>
     </div>
-  );
+  )
+        }
 }
-
 export default ShoppingList;
